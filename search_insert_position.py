4d634c4446439c7418da067e7cd46a5d94fbc3ff
Udasi Tharani <udasitharani@gmail.com>
2021-10-09 00:34:04 +0530

    search insert postition with binary search

--- search_insert_position.py
+++ search_insert_position.py
@@ -1,16 +1,18 @@
-# 1 2 3 5 6 7 8
+# 1 2 3
 
 from typing import List
 
 
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        for idx, val in enumerate(nums):
-            if val == target:
-                return idx
-            if target < val:
-                return idx
-        return len(nums)
+        if len(nums) == 1:
+            return 0 if nums[0] >= target else 1
+        mid = len(nums) // 2
+        if target == nums[mid]:
+            return mid
+        if target > nums[mid]:
+            return mid + self.searchInsert(nums[mid:], target)
+        return self.searchInsert(nums[:mid], target)
 
 
 tests = [
