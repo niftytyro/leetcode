b820ada97119ae3299fac2ce1242c9c0f5b68e31
Udasi Tharani <udasitharani@gmail.com>
2021-10-08 14:31:15 +0530

    The Two Sums problem with brute force method

---
+++ two_sum.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+# TODO Implement a solution better than O(n^2)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
+
+
+tests = [
+    (
+        ([2, 7, 11, 15], 9),
+        [0, 1],
+    ),
+    (
+        ([3, 2, 4], 6),
+        [1, 2],
+    ),
+    (
+        ([3, 3], 6),
+        [0, 1],
+    ),
+]
