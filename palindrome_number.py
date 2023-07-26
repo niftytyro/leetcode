5b23446a6d7bd9f723300a081db01d7e51a4dbad
Udasi Tharani <udasitharani@gmail.com>
2023-07-26 17:50:13 +0530

    add palindrome problem

---
+++ palindrome_number.py
@@ -0,0 +1,23 @@
+import math
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        reversed = str(x)[::-1]
+        return reversed == str(x)
+
+
+tests = [
+    (
+        (121,),
+        True,
+    ),
+    (
+        (-121,),
+        False,
+    ),
+    (
+        ( 10, ),
+        False
+    )
+]
+
